9e37be17051b77b51b0e6cd68cf5595d19d0b541
Nicholas Peters <nicholas.peters.external@eviden.com>
2024-05-24 11:58:32 +0100

    Add http handler for returning 500 errors

---
+++ src/EPR.ProducerContentValidation.FunctionApp/ErrorHttpTrigger.cs
@@ -0,0 +1,22 @@
+﻿namespace EPR.EventDispatcher.Functions
+{
+    using System.Net;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+
+    /// <summary>
+    /// Health check endpoint "api/error".
+    /// "/api/error" will return 500 Internal Serer Error.
+    /// </summary>
+    public static class ErrorHttpTrigger
+    {
+        [FunctionName(nameof(ErrorHttpTrigger))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "error")] HttpRequest req)
+        {
+            return new OkObjectResult("Error") { StatusCode = (int)HttpStatusCode.InternalServerError };
+        }
+    }
+}
