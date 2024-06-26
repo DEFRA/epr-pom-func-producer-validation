4cf61dc0335e01126fb8405ca75dd1e1e12b7f90
MasilEviden <masil.puveendran@eviden.com>
2024-06-26 16:30:01 +0100

    Upgraded to dotnet 8

--- src/EPR.ProducerContentValidation.Application/Exceptions/MissingSubmissionConfidurationException.cs
+++ src/EPR.ProducerContentValidation.Application/Exceptions/MissingSubmissionConfidurationException.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
 
 namespace EPR.ProducerContentValidation.Application.Exceptions;
 
@@ -9,11 +8,6 @@ public class MissingSubmissionConfidurationException : Exception
 {
     public MissingSubmissionConfidurationException(string message)
     : base(message)
-    {
-    }
-
-    protected MissingSubmissionConfidurationException(SerializationInfo info, StreamingContext context)
-    : base(info, context)
     {
     }
 }
