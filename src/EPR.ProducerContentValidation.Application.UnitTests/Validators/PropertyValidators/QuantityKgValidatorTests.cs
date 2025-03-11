08fbd39e2cfa28d83c60a38c82210dca7261cf33
Sharjeel Baloch <sharjeel.baloch.external@eviden.com>
2025-03-11 14:37:20 +0000

    Changed to correct errorCode

--- src/EPR.ProducerContentValidation.Application.UnitTests/Validators/PropertyValidators/QuantityKgValidatorTests.cs
+++ src/EPR.ProducerContentValidation.Application.UnitTests/Validators/PropertyValidators/QuantityKgValidatorTests.cs
@@ -37,7 +37,7 @@ public class QuantityKgValidatorTests
     [DataRow("25000")]
     public void QuantityKgValidator_Passes_When_MatchOtherZeroReturnsCondition_And_QuantityKg_IsNotZero(string quantityKg)
     {
-        var model = new ProducerRow(null, "2024-P0", "105761", 1, null, "L", "OW", "O2", "OT", "rubber", "SC", "EN", quantityKg, null, "January to June 2024");
+        var model = new ProducerRow(null, "2024-P0", "105761", 1, null, "L", "OW", "O2", "OT", "rubber", "SC", " ", quantityKg, null, "January to June 2024");
 
         // Act
         var result = _systemUnderTest.TestValidate(model);
