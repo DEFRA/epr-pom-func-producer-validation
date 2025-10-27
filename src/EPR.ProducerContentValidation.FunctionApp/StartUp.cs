436d48d6dcab9e79ba25e4efa0dee0e1547f4f76
To-Sanh Nim <156068367+tsnatos@users.noreply.github.com>
2025-10-27 15:16:08 +0000

    AB#613909 Add resilience POM (#68)
    * Add resilience
    
    * refactor
    
    * pr comments
    
    * refactor

--- src/EPR.ProducerContentValidation.FunctionApp/StartUp.cs
+++ src/EPR.ProducerContentValidation.FunctionApp/StartUp.cs
@@ -11,10 +11,12 @@ using Application;
 using EPR.ProducerContentValidation.Application.Clients;
 using EPR.ProducerContentValidation.Application.Config;
 using EPR.ProducerContentValidation.Application.Handlers;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
 using Microsoft.Extensions.Options;
 using Microsoft.FeatureManagement;
+using Polly;
+using Polly.Timeout;
 
 [ExcludeFromCodeCoverage]
 public class StartUp : FunctionsStartup
@@ -33,8 +35,48 @@ public class StartUp : FunctionsStartup
             var companyDetailsApiConfig = sp.GetRequiredService<IOptions<CompanyDetailsApiConfig>>().Value;
             c.BaseAddress = new Uri(companyDetailsApiConfig.BaseUrl);
             c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            c.Timeout = TimeSpan.FromSeconds(companyDetailsApiConfig.Timeout);
         })
-        .AddHttpMessageHandler<CompanyDetailsApiAuthorisationHandler>();
+        .AddHttpMessageHandler<CompanyDetailsApiAuthorisationHandler>()
+        .AddResilienceHandler("CompanyDetailsResiliencePipeline", BuildResiliencePipeline<CompanyDetailsApiConfig>(o => TimeSpan.FromSeconds(o.Timeout)));
+    }
+
+    private static Action<ResiliencePipelineBuilder<HttpResponseMessage>, ResilienceHandlerContext> BuildResiliencePipeline<TConfig>(Func<TConfig, TimeSpan> timeoutSelector)
+        where TConfig : class =>
+        (builder, context) =>
+        {
+            var sp = context.ServiceProvider;
+            var timeout = timeoutSelector(sp.GetRequiredService<IOptions<TConfig>>()?.Value);
+            BuildResiliencePipeline(builder, timeout);
+        };
+
+    private static void BuildResiliencePipeline(ResiliencePipelineBuilder<HttpResponseMessage> builder, TimeSpan? timeout = null)
+    {
+        builder.AddRetry(new HttpRetryStrategyOptions
+        {
+            MaxRetryAttempts = 4,
+            Delay = TimeSpan.FromSeconds(2),
+            BackoffType = DelayBackoffType.Exponential,
+            ShouldHandle = args =>
+            {
+                bool shouldHandle;
+                var exception = args.Outcome.Exception;
+                if (exception is TimeoutRejectedException ||
+                   (exception is OperationCanceledException && exception.Source == "System.Private.CoreLib" && exception.InnerException is TimeoutException))
+                {
+                    shouldHandle = true;
+                }
+                else
+                {
+                    shouldHandle = HttpClientResiliencePredicates.IsTransient(args.Outcome);
+                }
+
+                return new ValueTask<bool>(shouldHandle);
+            },
+        });
+
+        if (timeout is not null)
+        {
+            builder.AddTimeout(timeout.Value);
+        }
     }
 }
