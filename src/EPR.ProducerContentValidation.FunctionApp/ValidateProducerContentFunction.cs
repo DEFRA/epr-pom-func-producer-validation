cd1e3b30558608d2072da6f4da63d98f6fe829ed
Killian Davies <killian.davies@esynergy.co.uk>
2026-02-24 12:10:16 +0000

    reinstate sb func

--- src/EPR.ProducerContentValidation.FunctionApp/ValidateProducerContentFunction.cs
+++ src/EPR.ProducerContentValidation.FunctionApp/ValidateProducerContentFunction.cs
@@ -37,7 +37,7 @@ public class ValidateProducerContentFunction
         _logger = logger;
     }
 
-    // [Function("ValidateProducerContent")]
+    [Function("ValidateProducerContent")]
     public async Task RunAsync(
         [ServiceBusTrigger("%ServiceBus:SplitQueueName%", Connection = "ServiceBus:ConnectionString")]
         ProducerValidationInRequest producerValidationRequest)
