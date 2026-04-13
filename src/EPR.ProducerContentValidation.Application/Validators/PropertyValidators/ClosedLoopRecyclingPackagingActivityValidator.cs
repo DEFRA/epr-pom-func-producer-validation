2d1653387617552e40921a2617b4594ae7fa9604
Killian Davies <killian.davies@esynergy.co.uk>
2026-04-13 17:08:13 +0100

    initial revision - clr packaging activity validation

---
+++ src/EPR.ProducerContentValidation.Application/Validators/PropertyValidators/ClosedLoopRecyclingPackagingActivityValidator.cs
@@ -0,0 +1,31 @@
+using System.Collections.Immutable;
+using EPR.ProducerContentValidation.Application.Constants;
+using EPR.ProducerContentValidation.Application.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace EPR.ProducerContentValidation.Application.Validators.PropertyValidators;
+
+public class ClosedLoopRecyclingPackagingActivityValidator : AbstractValidator<ProducerRow>
+{
+    private readonly ImmutableList<string> _skipRuleErrorCodes = new List<string>()
+    {
+        ErrorCode.PackagingTypeInvalidErrorCode,
+        ErrorCode.ClosedLoopRecyclingPackagingTypeInvalidForSmallProducerErrorCode
+    }.ToImmutableList();
+
+    public ClosedLoopRecyclingPackagingActivityValidator()
+    {
+        RuleFor(x => x.ProducerType)
+            .Null()
+            .WithErrorCode(ErrorCode.ClosedLoopRecyclingPackagingActivityInvalidErrorCode);
+    }
+
+    protected override bool PreValidate(ValidationContext<ProducerRow> context, ValidationResult result)
+    {
+        var producerRow = context.InstanceToValidate;
+        return !result.Errors.Exists(x => _skipRuleErrorCodes.Contains(x.ErrorCode))
+               && PackagingType.ClosedLoopRecycling.Equals(producerRow.WasteType)
+               && producerRow.ProducerType != null;
+    }
+}
