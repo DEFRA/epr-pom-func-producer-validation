97d8f3b3fae158adb3006ce1a1516ffef5579513
Killian Davies <killian.davies@esynergy.co.uk>
2026-04-13 11:33:04 +0100

    Feature/sub 199 disallow clr pre july 2026 (#77)
    * initial revision - disallow CLR pre July 2026
    
    * correction - apply from P1 2026 onwards
    
    * correction for submission period format
    
    * sonarqube - additional coverage

---
+++ src/EPR.ProducerContentValidation.Application/Validators/PropertyValidators/ClosedLoopRecyclingSubmissionPeriodValidator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Immutable;
+using EPR.ProducerContentValidation.Application.Constants;
+using EPR.ProducerContentValidation.Application.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace EPR.ProducerContentValidation.Application.Validators.PropertyValidators;
+
+public class ClosedLoopRecyclingSubmissionPeriodValidator : AbstractValidator<ProducerRow>
+{
+    private readonly ImmutableList<string> _skipRuleErrorCodes = new List<string>()
+    {
+        ErrorCode.PackagingTypeInvalidErrorCode,
+        ErrorCode.ClosedLoopRecyclingPackagingTypeInvalidForSmallProducerErrorCode
+    }.ToImmutableList();
+
+    public ClosedLoopRecyclingSubmissionPeriodValidator()
+    {
+        RuleFor(x => x.DataSubmissionPeriod)
+            .Must(IsYearValidForClosedLoopRecycling)
+            .WithErrorCode(ErrorCode.ClosedLoopRecyclingSubmissionPeriodInvalidErrorCode);
+    }
+
+    protected override bool PreValidate(ValidationContext<ProducerRow> context, ValidationResult result)
+    {
+        var producerRow = context.InstanceToValidate;
+        return !result.Errors.Exists(x => _skipRuleErrorCodes.Contains(x.ErrorCode))
+               && PackagingType.ClosedLoopRecycling.Equals(producerRow.WasteType);
+    }
+
+    private static bool IsYearValidForClosedLoopRecycling(string? period) =>
+        period is { Length: >= 4 }
+        && int.TryParse(period.AsSpan(0, 4), out var year)
+        && year >= 2026;
+}
