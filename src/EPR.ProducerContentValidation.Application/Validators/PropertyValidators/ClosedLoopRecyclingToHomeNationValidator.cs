6a049fa211c2497f11c860ca9f563210a75e7036
Killian Davies <killian.davies@esynergy.co.uk>
2026-04-17 14:02:20 +0100

    initial revision - clr validation for "to country"

---
+++ src/EPR.ProducerContentValidation.Application/Validators/PropertyValidators/ClosedLoopRecyclingToHomeNationValidator.cs
@@ -0,0 +1,30 @@
+using System.Collections.Immutable;
+using EPR.ProducerContentValidation.Application.Constants;
+using EPR.ProducerContentValidation.Application.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace EPR.ProducerContentValidation.Application.Validators.PropertyValidators;
+
+public class ClosedLoopRecyclingToHomeNationValidator : AbstractValidator<ProducerRow>
+{
+    private readonly ImmutableList<string> _skipRuleErrorCodes = new List<string>()
+    {
+        ErrorCode.PackagingTypeInvalidErrorCode,
+        ErrorCode.ClosedLoopRecyclingPackagingTypeInvalidForSmallProducerErrorCode
+    }.ToImmutableList();
+
+    public ClosedLoopRecyclingToHomeNationValidator()
+    {
+        RuleFor(x => x.ToHomeNation)
+            .Null()
+            .WithErrorCode(ErrorCode.ClosedLoopRecyclingToHomeNationInvalidErrorCode);
+    }
+
+    protected override bool PreValidate(ValidationContext<ProducerRow> context, ValidationResult result)
+    {
+        var producerRow = context.InstanceToValidate;
+        return !result.Errors.Exists(x => _skipRuleErrorCodes.Contains(x.ErrorCode))
+               && PackagingType.ClosedLoopRecycling.Equals(producerRow.WasteType);
+    }
+}
